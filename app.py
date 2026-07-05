88be7241cf3d561d5805994574bf7b1e530ac2f6
kpiya550-hub <kpiya550@gmail.com>
2026-07-05 15:40:24 +0530

    day12-Contact from Database

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ from routes.services import services
 from routes.gallery import gallery
 from routes.portfolio import portfolio
 from routes.contact import contact
+from models.contact import db
 
 
 app = Flask(__name__)   # Pehle app banao
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rajshree.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db.init_app(app)
 app.register_blueprint(about)   # Fir blueprint register karo
 app.register_blueprint(services)
 app.register_blueprint(gallery)
 app.register_blueprint(portfolio)
 app.register_blueprint(contact)
-
+with app.app_context():
+    db.create_all()
 @app.route("/")
 def home():
     return render_template("index.html")
--- routes/contact.py
+++ routes/contact.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from models.contact import Contact, db
 
 contact = Blueprint("contact", __name__)
 
-@contact.route("/contact")
+@contact.route("/contact", methods=["GET", "POST"])
 def contact_page():
+
+    if request.method == "POST":
+
+        new_contact = Contact(
+            name=request.form["name"],
+            email=request.form["email"],
+            phone=request.form["phone"],
+            message=request.form["message"]
+        )
+
+        db.session.add(new_contact)
+        db.session.commit()
+
+        return redirect("/contact")
+
     return render_template("contact.html")
